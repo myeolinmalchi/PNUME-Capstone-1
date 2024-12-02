50c8e7ded0ade224e04a828d687b1b00261e8a28
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-12-02 23:38:42 +0900

    feat: `kpts` 계산 로직 제거

--- m1s/utils/yolov8.py
+++ m1s/utils/yolov8.py
@@ -60,6 +60,18 @@ class YOLOv8:
             img = cv2.resize(srcimg, (self.input_width, self.input_height), interpolation=cv2.INTER_AREA)
         return img, newh, neww, top, left
 
+    def detect_largest_face(self, img):
+        boxes, scores, _ = self.detect(img)
+        idx, max_size = -1, 0
+        for i, box in enumerate(boxes):
+            _, _, w, h = box.astype(int)
+            size = w * h
+            if size > max_size:
+                max_size = size
+                idx = i
+        dstimg, center_x, center_y = self.draw_detection(img, boxes[idx], scores[idx])
+        return dstimg, center_x, center_y
+
     def detect(self, srcimg):
         input_img, newh, neww, padh, padw = self.resize_image(cv2.cvtColor(srcimg, cv2.COLOR_BGR2RGB))
         scale_h, scale_w = srcimg.shape[0]/newh, srcimg.shape[1]/neww
@@ -67,23 +79,21 @@ class YOLOv8:
 
         input_img = np.expand_dims(input_img, 0)
         outputs = self.model.run([input_img])
-        # if isinstance(outputs, tuple):
-        #     outputs = list(outputs)
-        # if float(cv2.__version__[:3])>=4.7:
-        #     outputs = [outputs[2], outputs[0], outputs[1]] ###opencv4.7需要这一步，opencv4.5不需要
-        # Perform inference on the image
-        det_bboxes, det_conf, det_classid, landmarks = self.post_process(outputs, scale_h, scale_w, padh, padw)
-        return det_bboxes, det_conf, det_classid, landmarks
+        # det_bboxes, det_conf, det_classid, landmarks = self.post_process(outputs, scale_h, scale_w, padh, padw)
+        # return det_bboxes, det_conf, det_classid, landmarks
+        det_bboxes, det_conf, det_classid = self.post_process(outputs, scale_h, scale_w, padh, padw)
+        return det_bboxes, det_conf, det_classid
 
+    # TODO: Cython으로 변환해서 후처리 속도 개선
     def post_process(self, preds, scale_h, scale_w, padh, padw):
-        bboxes, scores, landmarks = [], [], []
-        for i, pred in enumerate(preds):
+        bboxes, scores = [], []
+        for pred in preds:
             stride = int(self.input_height/pred.shape[2])
             pred = pred.transpose((0, 2, 3, 1))
             
             box = pred[..., :self.reg_max * 4]
             cls = 1 / (1 + np.exp(-pred[..., self.reg_max * 4:-15])).reshape((-1,1))
-            kpts = pred[..., -15:].reshape((-1,15)) ### x1,y1,score1, ..., x5,y5,score5
+            #kpts = pred[..., -15:].reshape((-1,15)) ### x1,y1,score1, ..., x5,y5,score5
 
             # tmp = box.reshape(self.feats_hw[i][0], self.feats_hw[i][1], 4, self.reg_max)
             tmp = box.reshape(-1, 4, self.reg_max)
@@ -91,22 +101,22 @@ class YOLOv8:
             bbox_pred = np.dot(bbox_pred, self.project).reshape((-1,4))
 
             bbox = self.distance2bbox(self.anchors[stride], bbox_pred, max_shape=(self.input_height, self.input_width)) * stride
-            kpts[:, 0::3] = (kpts[:, 0::3] * 2.0 + (self.anchors[stride][:, 0].reshape((-1,1)) - 0.5)) * stride
-            kpts[:, 1::3] = (kpts[:, 1::3] * 2.0 + (self.anchors[stride][:, 1].reshape((-1,1)) - 0.5)) * stride
-            kpts[:, 2::3] = 1 / (1+np.exp(-kpts[:, 2::3]))
+            #kpts[:, 0::3] = (kpts[:, 0::3] * 2.0 + (self.anchors[stride][:, 0].reshape((-1,1)) - 0.5)) * stride
+            #kpts[:, 1::3] = (kpts[:, 1::3] * 2.0 + (self.anchors[stride][:, 1].reshape((-1,1)) - 0.5)) * stride
+            #kpts[:, 2::3] = 1 / (1+np.exp(-kpts[:, 2::3]))
 
             bbox -= np.array([[padw, padh, padw, padh]])  ###合理使用广播法则
             bbox *= np.array([[scale_w, scale_h, scale_w, scale_h]])
-            kpts -= np.tile(np.array([padw, padh, 0]), 5).reshape((1,15))
-            kpts *= np.tile(np.array([scale_w, scale_h, 1]), 5).reshape((1,15))
+            #kpts -= np.tile(np.array([padw, padh, 0]), 5).reshape((1,15))
+            #kpts *= np.tile(np.array([scale_w, scale_h, 1]), 5).reshape((1,15))
 
             bboxes.append(bbox)
             scores.append(cls)
-            landmarks.append(kpts)
+            #landmarks.append(kpts)
 
         bboxes = np.concatenate(bboxes, axis=0)
         scores = np.concatenate(scores, axis=0)
-        landmarks = np.concatenate(landmarks, axis=0)
+        #landmarks = np.concatenate(landmarks, axis=0)
     
         bboxes_wh = bboxes.copy()
         bboxes_wh[:, 2:4] = bboxes[:, 2:4] - bboxes[:, 0:2]  ####xywh
@@ -117,18 +127,19 @@ class YOLOv8:
         bboxes_wh = bboxes_wh[mask]  ###合理使用广播法则
         confidences = confidences[mask]
         classIds = classIds[mask]
-        landmarks = landmarks[mask]
+        #landmarks = landmarks[mask]
         
         indices = np.asarray(cv2.dnn.NMSBoxes(bboxes_wh.tolist(), confidences.tolist(), self.conf_threshold, self.iou_threshold)).flatten()
         if len(indices) > 0:
             mlvl_bboxes = bboxes_wh[indices]
             confidences = confidences[indices]
             classIds = classIds[indices]
-            landmarks = landmarks[indices]
-            return mlvl_bboxes, confidences, classIds, landmarks
+            # landmarks = landmarks[indices]
+            # return mlvl_bboxes, confidences, classIds, landmarks
+            return mlvl_bboxes, confidences, classIds 
         else:
             print('nothing detect')
-            return np.array([]), np.array([]), np.array([]), np.array([])
+            return np.array([]), np.array([]), np.array([])
 
     def distance2bbox(self, points, distance, max_shape=None):
         x1 = points[:, 0] - distance[:, 0]
@@ -142,13 +153,13 @@ class YOLOv8:
             y2 = np.clip(y2, 0, max_shape[0])
         return np.stack([x1, y1, x2, y2], axis=-1)
 
-    def draw_detection(self, image, box, score, kp):
+    def draw_detection(self, image, box, score):
         x, y, w, h = box.astype(int)
+        center_pos = (x + (w / 2), y + (h / 2))
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
         cv2.putText(image, "face:"+str(round(score,2)), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
-        for i in range(5):
-            cv2.circle(image, (int(kp[i * 3]), int(kp[i * 3 + 1])), 4, (0, 255, 0), thickness=-1)
-        return image
+        cv2.circle(image, (center_pos[0], center_pos[1]), 4, (0, 255, 0), thickness=-1)
+        return image, center_pos[0], center_pos[1]
     
     def draw_detections(self, image, boxes, scores, kpts):
         for box, score, kp in zip(boxes, scores, kpts):
