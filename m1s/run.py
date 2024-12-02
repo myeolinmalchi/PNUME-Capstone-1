1ca8b6811d0e7a757dc2a0fec99e4281619f5f05
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-12-02 23:39:18 +0900

    feat: 추상화된 메소드 호출하게끔 변경

--- m1s/run.py
+++ m1s/run.py
@@ -1,35 +1,31 @@
 from utils.yolov8 import YOLOv8
 from utils.wrappers import ModelWrapper
 from utils.camera import setup_camera
+
+import serial
 import cv2
 
 MODEL_PATH = './models/yolov8n-face.rknn'
 CAM_WIDTH = 640
 CAM_HEIGHT = 640
 
+py_serial = serial.Serial(
+    port='COM3',
+    baudrate=9600
+)
+
 if __name__ == '__main__':
     model, _ = ModelWrapper.setup(MODEL_PATH)
-    yolov8 = YOLOv8(model)
     capture = setup_camera(CAM_WIDTH, CAM_HEIGHT)
+    yolov8 = YOLOv8(model)
 
     while cv2.waitKey(1) < 0:
         status, frame = capture.read()
-        
         if not status:
             break
 
-        boxes, scores, classids, kpts = yolov8.detect(frame)
-        idx = -1
-        max_size = 0
-        for i, box in enumerate(boxes):
-            x, y, w, h = box
-            size = w * h
-            if size > max_size:
-                max_size = size
-                idx = i
-        dstimg = yolov8.draw_detection(frame, boxes[idx], scores[idx], kpts[idx])
-        cv2.imshow('Webcam', dstimg)
-        
+        result_img, x, y = yolov8.detect_largest_face(frame)
+        cv2.imshow('Webcam', result_img)
 
     # release
     model.release()
