c672f42e56125885b33ded78236cc43a9edb62fa
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-12-02 23:51:13 +0900

    feat: 시리얼 통신으로 아두이노에 얼굴 좌표값 전송

--- m1s/run.py
+++ m1s/run.py
@@ -9,14 +9,17 @@ MODEL_PATH = './models/yolov8n-face.rknn'
 CAM_WIDTH = 640
 CAM_HEIGHT = 640
 
-py_serial = serial.Serial(
-    port='COM3',
-    baudrate=9600
-)
+SERIAL_PORT = 'COM3'
+SERIAL_BAUDRATE = 9600
+
 
 if __name__ == '__main__':
     model, _ = ModelWrapper.setup(MODEL_PATH)
     capture = setup_camera(CAM_WIDTH, CAM_HEIGHT)
+    arduino = serial.Serial(
+        port=SERIAL_PORT,
+        baudrate=SERIAL_BAUDRATE
+    )
     yolov8 = YOLOv8(model)
 
     while cv2.waitKey(1) < 0:
@@ -25,6 +28,7 @@ if __name__ == '__main__':
             break
 
         result_img, x, y = yolov8.detect_largest_face(frame)
+        arduino.write(f"{x},{y}\n".encode())
         cv2.imshow('Webcam', result_img)
 
     # release
