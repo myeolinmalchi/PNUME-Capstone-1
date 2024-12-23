de1802134265b795815f53fba7256e154c4180c5
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-12-23 18:17:06 +0900

    feat: `rknnlite` 인스턴스 다중 실행

--- m1s/rknn.py
+++ m1s/rknn.py
@@ -5,24 +5,36 @@ from cv2.typing import MatLike
 
 from utils.wrappers import RKNNWrapper
 
+
+def init_rknns(model_path: str, N: int):
+    rknns = []
+    for _ in range(N):
+        rknns.append(RKNNWrapper(model_path))
+    return rknns
+
+        
 class RKNNPoolExecutor():
     def __init__(self, model_path: str, N: int, func):
         self.queue = Queue()
-        self.rknn = RKNNWrapper(model_path)
+        self.rknns = init_rknns(model_path, N)
         self.pool = ThreadPoolExecutor(max_workers=N)
         self.func = func
+        self.num = 0
+        self.N = N
 
     def put(self, frame: MatLike):
-        future = self.pool.submit(self.func, self.rknn, frame)
+        future = self.pool.submit(self.func, self.rknns[self.num % self.N], frame)
         self.queue.put(future)
+        self.num += 1
 
     def get(self):
         if self.queue.empty():
-            return [], []
+            return None, None
         future = self.queue.get()
         result = future.result()
         return result
 
     def release(self):
         self.pool.shutdown()
-        self.rknn.release
+        for rknn in self.rknns:
+            rknn.release()
